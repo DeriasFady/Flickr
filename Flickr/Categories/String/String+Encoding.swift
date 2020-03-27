90dbd153ed585851bc1aad75721adce293a16a20
Fady Derias <fady.derias@andela.com>
2020-03-27 10:37:21 +0200

    FEAT: Creating a string extension that would add percent encoding for a string value

---
+++ Flickr/Categories/String/String+Encoding.swift
@@ -0,0 +1,18 @@
+//
+//  String+Encoding.swift
+//  Flickr
+//
+//  Created by Fady Derias on 3/26/20.
+//  Copyright © 2020 Andela. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func addingPercentEncodingForURLQueryValue() -> String? {
+        let allowedCharacters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~")
+        
+        return self.addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    }
+}
